e49a5a34e979ff9d0d12242915cc2bcbe6449c67
piyushchawla05 <piyush.chawla05@gmail.com>
2020-04-14 23:54:06 -0500

    added ! in app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi There!!Hope are you doing good!!
+          Hi There!!!Hope are you doing good!!
         </p>
       </div>
     );
